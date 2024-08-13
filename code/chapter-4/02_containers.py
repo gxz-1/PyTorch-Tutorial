ebf2415a5e13e837cc0265fa667539d6ac026751
213titanV <address@mail.com>
2024-08-13 10:55:19 +0800

    api

--- code/chapter-4/02_containers.py
+++ code/chapter-4/02_containers.py
@@ -11,16 +11,27 @@ from torchvision.models import alexnet
 
 
 if __name__ == "__main__":
-    # ========================== Sequential ==========================
+    # ==========================1. Sequential ==========================
     model = alexnet(pretrained=False)
+    # alexnet中用Sequential定义了多个模块的组合
+    #     self.classifier = nn.Sequential(
+    #     nn.Dropout(),
+    #     nn.Linear(256 * 6 * 6, 4096),
+    #     nn.ReLU(inplace=True),
+    #     nn.Dropout(),
+    #     nn.Linear(4096, 4096),
+    #     nn.ReLU(inplace=True),
+    #     nn.Linear(4096, num_classes),
+    # )
     fake_input = torch.randn((1, 3, 224, 224))
     output = model(fake_input)
 
-    # ========================== ModuleList ==========================
+    # ==========================2. ModuleList ==========================
 
     class MyModule(nn.Module):
         def __init__(self):
             super(MyModule, self).__init__()
+            #使用ModuleList生成10个线性层,不能直接用list
             self.linears = nn.ModuleList([nn.Linear(10, 10) for i in range(10)])
             # self.linears = [nn.Linear(10, 10) for i in range(10)]    # 观察model._modules，将会是空的
 
@@ -34,10 +45,11 @@ if __name__ == "__main__":
     output = model(fake_input)
     print(output.shape)
 
-    # ========================== ModuleDict ==========================
+    # ==========================3. ModuleDict ==========================
     class MyModule2(nn.Module):
         def __init__(self):
             super(MyModule2, self).__init__()
+            # 使用ModuleDict定义字典类型的模型集合，按需使用
             self.choices = nn.ModuleDict({
                     'conv': nn.Conv2d(3, 16, 5),
                     'pool': nn.MaxPool2d(3)
